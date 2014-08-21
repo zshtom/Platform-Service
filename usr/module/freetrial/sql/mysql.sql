68f4d7be29275edd4274e5d332ddada6145c8254
chunhuili <2947680759@qq.com>
2014-08-21 18:04:13 +0800

    update

--- usr/module/freetrial/sql/mysql.sql
+++ usr/module/freetrial/sql/mysql.sql
@@ -3,19 +3,11 @@ CREATE TABLE `{freetrial}` (
   `name`        VARCHAR(64)             NOT NULL DEFAULT '',
   `title`        VARCHAR(64)             NOT NULL DEFAULT '',
   `company`      VARCHAR(64)             NOT NULL DEFAULT '',
-<<<<<<< HEAD
   `email`        VARCHAR(64)             NOT NULL DEFAULT '',
   `phone`        VARCHAR(11)             NOT NULL DEFAULT '',
 
 
-  PRIMARY KEY        (`id`),
-  UNIQUE KEY         (`name`)
+  PRIMARY KEY        (`id`)
+
 
 );
-=======
-  `comp_mail`      VARCHAR(64)             NOT NULL DEFAULT '',
-  `phonenum`        INT(11) UNSIGNED         NOT NULL ,
-  
-   PRIMARY KEY        (`id`)
-);
->>>>>>> upstream/master
