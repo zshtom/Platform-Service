f965164789613100b49eb0c6ea992e04dba79f67
chunhuili <lichunhui@social-touch.com>
2014-09-10 11:12:38 +0800

    resource module

---
+++ usr/module/resource/config/nav.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Pi Engine (http://www.social-touch.com/)
+ * @link      http://www.social-touch.com/ for the Pi Engine source repository
+ * @copyright Copyright (c) Pi Engine (http://www.social-touch.com/)
+ */
+
+return array(
+    'item'    => array(
+        'admin'   => array(
+            'pagea'   => array(
+                'label'      => __('Index'),
+                'route'      => 'admin',
+                'controller' => 'index',
+                'action'     => 'index',
+            ),
+            'pageb'   => array(
+                'label'      => __('Add'),
+                'route'      => 'admin',
+                'controller' => 'index',
+                'action'     => 'add',
+            )
+        ),
+    ),
+);
