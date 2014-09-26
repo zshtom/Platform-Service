acfda246d4dfe6df62fcf0362c7dce270cf36059
zhangtao <ztok999@163.com>
2014-09-26 13:28:05 +0800

    update solution add can't select checkbox and fix freetrial btn color

--- usr/theme/social/config.php
+++ usr/theme/social/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'version'       => '1.0.0-beta.1',
-    'type'          => 'front',
+    'type'          => 'both',
     'title'         => 'social Theme',
     'author'        => 'zto <zhangtao@social-touche.com>',
     'screenshot'    => 'image/screenshot.png',
