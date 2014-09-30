d67cc49b5927e23de798c53f25be32abfb269f62
Yuan Di <yuandi@social-touch.com>
2014-09-30 10:33:17 +0800

    Optimize the custom navigation and add translation for it

--- usr/module/apps/config/nav.php
+++ usr/module/apps/config/nav.php
@@ -39,54 +39,6 @@ return array(
 
         // Custom navigation
         'site' => array(
-            'nav-home' => array(
-                'order'         => -100,
-                'label'         => _a('Home'),
-                'route'         => 'home',
-
-                'pages'         => array(
-                    'account'   => array(
-                        'label'         => _a('Profile'),
-                        'route'         => 'sysuser',
-                        'controller'    => 'profile',
-
-                        'pages'         => array(
-                            'login'     => array(
-                                'label'         => _a('Login'),
-                                'route'         => 'sysuser',
-                                'controller'    => 'login',
-                                'visible'       => 0,
-                            ),
-
-                            'register'     => array(
-                                'label'         => _a('Register'),
-                                'route'         => 'sysuser',
-                                'controller'    => 'register',
-                                'visible'       => 0,
-                            ),
-
-                            'password'     => array(
-                                'label'         => _a('Password'),
-                                'route'         => 'sysuser',
-                                'controller'    => 'password',
-                                'visible'       => 0,
-                            ),
-                        ),
-                    ),
-                    'admin'     => array(
-                        'label'     => _a('Admin'),
-                        'route'     => 'home',
-                        'section'   => 'admin',
-                        'target'    => '_blank',
-                    ),
-                    'feed'     => array(
-                        'label'     => _a('RSS Feed'),
-                        'route'     => 'feed',
-                        'section'   => 'feed',
-                        'target'    => '_blank',
-                    ),
-                ),
-            ),
             'modules'   => array(
                 'callback'  => array('navigation', 'front'),
             ),
